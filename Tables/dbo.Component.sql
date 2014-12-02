46faf969b1898c898089e0f7566f5c9018666676
James Billings <james.billings@red-gate.com>
2014-12-02 10:42:57 +0000

    gdfgfdgdfgdf

--- Tables/dbo.Component.sql
+++ Tables/dbo.Component.sql
@@ -5,7 +5,8 @@ CREATE TABLE [dbo].[Component]
 [Componentnotes] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ComponentTolerance] [int] NULL,
 [ComponentNewCol] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ewettwe] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[ewettwe] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[vnjfg] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Component] ADD CONSTRAINT [PK_Component] PRIMARY KEY CLUSTERED  ([ComponentID]) ON [PRIMARY]
