30cc0e9dfd0f8dc22fc85d66d692a68c9685d9d6
James Billings <james.billings@red-gate.com>
2014-12-02 10:39:12 +0000

    dfsgdff

--- Tables/dbo.Component.sql
+++ Tables/dbo.Component.sql
@@ -4,15 +4,7 @@ CREATE TABLE [dbo].[Component]
 [ComponentDescription] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Componentnotes] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ComponentTolerance] [int] NULL,
-[ComponentNewCol] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[dfbfdbd] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[dsfgdfg] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[afsdgsgsd] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[dfvdvbb] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[dfgnrfgfgnfgn] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[fgkyfj] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[yjyukrfkur] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[edhseertt] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[ComponentNewCol] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Component] ADD CONSTRAINT [PK_Component] PRIMARY KEY CLUSTERED  ([ComponentID]) ON [PRIMARY]
