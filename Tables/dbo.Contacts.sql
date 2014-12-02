e6ae2649b176af73646af275b04c600eab9337cc
James Billings <james.billings@red-gate.com>
2014-12-02 09:59:02 +0000

    Add twitter column to contact

--- Tables/dbo.Contacts.sql
+++ Tables/dbo.Contacts.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[Contacts]
 [Address2] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Address3] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [JoiningDate] [datetime] NULL CONSTRAINT [DF__Contacts__Joinin__164452B1] DEFAULT (getdate()),
-[Email] [nvarchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Email] [nvarchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Twitter] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Contacts] ADD CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
