451fe69582d7b1c325dbe3fe81cc19e477bbdb1a
James Billings <james.billings@red-gate.com>
2015-12-04 15:39:03 +0000

    no message

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Table_1]
 (
 [id] [int] NOT NULL,
 [thing] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[anotherthing] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[anotherthing] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[moarthing] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_1] ADD CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED  ([id]) ON [PRIMARY]
